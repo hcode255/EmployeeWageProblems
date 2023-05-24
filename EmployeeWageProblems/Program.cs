9431a545d5d7d0fc9cf2229bde6fb9a34523bc2d
Harsha <vharsha255@gmail.com>
2023-05-25 00:54:01 +0530

    hasha

--- EmployeeWageProblems/Program.cs
+++ EmployeeWageProblems/Program.cs
@@ -11,6 +11,7 @@ namespace EmployeeWageProblems
         static void Main(string[] args)
         {
             //constants
+            int IS_PART_TIME = 1;
             int IS_FULL_TIME = 1;
             int EMP_RATE_PER_HOUR = 20;
             //Variables
@@ -18,14 +19,18 @@ namespace EmployeeWageProblems
             int empWage = 0;
             Random random = new Random();
             //Computation
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
             }
             else
             {
-                empHrs = 0;
+                empHrs = 8;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage : " + empWage);
